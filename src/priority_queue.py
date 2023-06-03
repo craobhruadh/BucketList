6f1eea1661220f4dc740835f958e11e85e0b1830
Ross <publius314@gmail.com>
2023-06-03 11:25:03 -0700

    saving work, adding pre-commit hooks

--- src/priority_queue.py
+++ src/priority_queue.py
@@ -5,31 +5,24 @@ from typing import AnyStr
 
 @dataclass(order=True)
 class BucketItem:
-    priority: int
     item: AnyStr = field(compare=False)
+    priority: int = 1
 
 
 class BucketList:
-
     def __init__(self, initial_elems=[]):
         self.queue = []
         self.tasks = {}
 
         for elem in initial_elems:
             self.add(elem)
-        # self.min_priority = 0
-        # self.max_priority = 0
 
     def add(self, task: str, priority: int = 1):
-        # self.max_priority += 1
         if task in self.tasks.keys():
             self.remove(task)
 
-        entry = (priority, task)
-        heapq.heappush(
-            self.queue,
-            entry
-        )
+        entry = BucketItem(task, priority)
+        heapq.heappush(self.queue, entry)
         self.tasks[task] = entry
         heapq.heapify(self.queue)
 
@@ -38,7 +31,7 @@ class BucketList:
 
     def get(self):
         """Get the current highest priority task"""
-        return self.queue[0][1]
+        return self.queue[0].item
 
     def finish(self, task: str):
         """Finish a task?  Great job!  Remove it from the queue"""
@@ -48,7 +41,7 @@ class BucketList:
     def prioritize(self, task):
         """Have an itch to do something?  Move it
         to the top of thr queue"""
-        pass#self.add(task)
+        pass  # self.add(task)
 
     @property
     def n_items(self):
--- tests/test_priority_queue.py
+++ tests/test_priority_queue.py
@@ -1,38 +1,31 @@
-from src.priority_queue import BucketList
+from src.priority_queue import BucketList, BucketItem
+
+
+def test_BucketItem():
+    item_1 = BucketItem(item="travel the world")
+    item_2 = BucketItem(item="clean my apartment", priority=2)
+    assert item_1.item == "travel the world"
+    assert item_2.item == "clean my apartment"
+    assert item_1.priority == 1
+    assert item_2.priority == 2
 
 
 def test_heap():
     bucket_list = BucketList()
-    bucket_list.add('travel world')
-    bucket_list.add('write a book')
-    bucket_list.add('save the world')
+    bucket_list.add("travel world")
+    bucket_list.add("write a book")
+    bucket_list.add("save the world")
 
     assert bucket_list.n_items == 3
-    assert bucket_list.get() == 'save the world'
+    assert bucket_list.get() == "save the world"
 
-    bucket_list.prioritize('travel world')
-
+    bucket_list.prioritize("travel world")
     bucket_list.print()
     # bucket_list.prioritize('save the world')
-    assert bucket_list.get() == 'travel world'
+    assert bucket_list.get() == "travel world"
 
 
 def test_init_heap():
-    bucket_list = BucketList(
-        [
-            'stuff to do',
-            'more to do',
-            'even more to do'
-        ]
-    )
-    assert bucket_list.get() == 'even more to do'
+    bucket_list = BucketList(["stuff to do", "more to do", "even more to do"])
+    assert bucket_list.get() == "even more to do"
     assert bucket_list.n_items == 3
-
-
-# def test_validate():
-#     bucket_list = BucketList()
-#     assert (bucket_list.validate((0.1, 'Test this function')))
-#     assert (not bucket_list.validate(('0.1', 'Test this function')))
-#     assert (not bucket_list.validate(
-#         [(0.1, 'This should fail'), (0.2, 'This should really fail')]
-#     ))
